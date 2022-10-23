95fdf59d4235202873fff5f977bde223cefbaf45
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-10-23 19:04:01 -0400

    add first pack of icons

---
+++ src/assets/icons/AlignJustified.js
@@ -0,0 +1,11 @@
+import SvgIcon from '@mui/material/SvgIcon';
+
+export default function Again(props) {
+    return (
+        <SvgIcon {...props}>
+            <path d="M4 6H20" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M4 12H20" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M4 18H16" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </SvgIcon>
+    )
+}
---
+++ src/assets/icons/Emojies.js
@@ -0,0 +1,12 @@
+import SvgIcon from '@mui/material/SvgIcon';
+
+export default function Emojies(props) {
+    return (
+        <SvgIcon {...props}>
+            <path d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M9 9H9.01" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M15 9H15.01" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M8.3342 14.6006C8.53229 15.0543 8.81474 15.4716 9.17157 15.8284C9.92172 16.5786 10.9391 17 12 17C13.0609 17 14.0783 16.5786 14.8284 15.8284C15.1679 15.489 15.44 15.0823 15.6363 14.6461" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </SvgIcon>
+    )
+}
