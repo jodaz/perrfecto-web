5e8c56026468b5da337f320c5dcad36df02ce62f
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-10-31 00:05:17 -0400

    layout icons

--- src/components/FeedCard/index.js
+++ src/components/FeedCard/index.js
@@ -3,11 +3,16 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-// Samples
+import Badge from '@mui/material/Badge';
+
+// Icons
+import PawIcon from '../../assets/icons/Huella2.png'
+import { ReactComponent as StarIcon } from '../../assets/icons/Star.svg'
+import CloseIcon from '../../assets/icons/Close'
 
 export default function FeedCard() {
     return (
@@ -27,11 +32,11 @@ export default function FeedCard() {
                     border: '1.6875px solid #F59E0B'
                 }} src='/images/samples/sexy-woman.png' />
                 <Typography gutterBottom variant="h5" component="div">
-                    Lizard
+                    Carmen Pires
                 </Typography>
             </Box>
             <Card sx={{
-                height: 500,
+                height: 475,
                 maxWidth: 344,
                 borderRadius: '20px',
                 position: 'relative',
@@ -58,15 +63,71 @@ export default function FeedCard() {
                         display: 'flex',
                         justifyContent: 'space-between'
                     }}>
-                        <Button variant="contained" size="small">S</Button>
-                        <Button variant="contained" size="small">S</Button>
-                        <Button variant="contained" size="small">S</Button>
+                        <IconButton sx={{
+                            background: 'linear-gradient(180deg, #8C8C8C 0%, #4B4B4B 100%)',
+                            padding: '1rem'
+                        }}>
+                            <CloseIcon color="#fff" />
+                        </IconButton>
+                        <IconButton sx={{
+                            background: 'linear-gradient(180deg, #F8DD62 0%, #F5AA50 100%)',
+                            padding: '1rem'
+                        }}>
+                            <StarIcon />
+                        </IconButton>
+                        <Badge
+                            badgeContent={15}
+                            color="primary"
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'right'
+                            }}
+                            sx={{
+                                '& .MuiBadge-badge': {
+                                    height: '25px !important',
+                                    width: '25px !important',
+                                    borderRadius: '100px',
+                                    color: '#fff',
+                                    backgroundColor: theme => theme.palette.warning.main
+                                }
+                            }}
+                        >
+                            <IconButton sx={{
+                                background: 'linear-gradient(135deg, #F6C056 0%, #F65555 49.48%, #CC295F 100%)',
+                                padding: '0.6rem'
+                            }}>
+                                <img src={PawIcon} />
+                            </IconButton>
+                        </Badge>
                     </CardActions>
-                    <Box marginTop='2rem'>
-                        <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
+                    <Box sx={{
+                        marginTop: '2rem',
+                        textAlign: 'center'
+                    }}>
+                        <Typography variant="h5" color="text.primary">
+                            Pupi
                         </Typography>
+                        <Box sx={{
+                            display: 'flex',
+                            justifyContent: 'space-around',
+                            width: '80%',
+                            margin: '0 auto'
+                        }}>
+                            {/** Raza */}
+                            <Typography color="text.secondary">
+                                Pug
+                            </Typography>
+                            <Box>.</Box>
+                            {/** Edad */}
+                            <Typography color="text.secondary">
+                                3 años
+                            </Typography>
+                            <Box>.</Box>
+                            {/** Ubicación */}
+                            <Typography color="text.secondary">
+                                Sevilla, España
+                            </Typography>
+                        </Box>
                     </Box>
                 </CardContent>
             </Card>
