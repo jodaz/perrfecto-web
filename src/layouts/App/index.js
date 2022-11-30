76f453e9a08c9aa27ff380a206840364d524a4cd
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-11-29 23:48:00 -0400

    responsive home

--- src/layouts/App/index.js
+++ src/layouts/App/index.js
@@ -5,16 +5,35 @@ import { useAuth } from '../../context/AuthContext'
 // Screens
 import BusinessHome from './BusinessHome';
 import UsersHome from './UsersHome';
+import { useMediaQuery } from '@mui/material';
+import Navigation from './Navigation';
 
 export default function AppLayout({ children }) {
     const { state: { user } } = useAuth();
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
 
     return (
-        <Box sx={{ display: 'flex' }}>
-            <Aside>
-                {children}
-            </Aside>
+        <Box sx={{
+            position: 'relative',
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            {(!isSmall) && (
+                <Aside>
+                    {children}
+                </Aside>
+            )}
             {(user.role == 'user') ? <UsersHome /> : <BusinessHome />}
+            {(isSmall) && (
+                <Box sx={{
+                    position: 'fixed',
+                    width: '100%',
+                    bottom: 0,
+                    left: 0,
+                }}>
+                    <Navigation />
+                </Box>
+            )}
         </Box>
     );
 }
