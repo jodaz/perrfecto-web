159863da30134fe75f7fc7df45f72c080a9ba295
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-12-08 23:26:12 -0400

    add ellipses

--- src/components/Modals/RegisterBusiness/index.js
+++ src/components/Modals/RegisterBusiness/index.js
@@ -9,11 +9,25 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 
 const Link = styled(MuiLink)(({ theme }) => ({
     color: theme.palette.primary.contrastText,
-    fontWeight: 500,
+    fontWeight: 600,
     '&:hover': {
         textDecoration: 'underline'
     }
 }));
+
+const EllipseComponent = ({
+    n,
+    sx
+}) => (
+    <Box
+        component='img'
+        src={`/images/default/Ellipse${n}.png`}
+        sx={{
+            position: 'absolute',
+            ...sx
+        }}
+    />
+)
 
 const RegisterBusiness = ({ location }) => {
     const isSmall = useMediaQuery((theme) => theme.breakpoints.down('sm'));
@@ -34,10 +48,25 @@ const RegisterBusiness = ({ location }) => {
                     <Box sx={{
                         flex: 1,
                         backgroundColor: '#A167C9',
-                        color: '#fff'
+                        color: '#fff',
+                        fontWeight: 400,
+                        position: 'relative'
                     }}>
+                        <EllipseComponent sx={{ bottom: 0, right: 0 }} n={9} />
+                        <EllipseComponent sx={{ right: '50px', top: '550px' }} n={8} />
+                        <EllipseComponent sx={{ right: '50px', top: '550px' }} n={8} />
+                        <EllipseComponent sx={{ right: '50px', top: '350px' }} n={7} />
+                        <EllipseComponent sx={{ right: 0, top: '50px' }} n={6} />
+                        <EllipseComponent sx={{ top: '200px', left: '150px' }} n={5} />
+                        <EllipseComponent sx={{ top: '450px', left: '50px' }} n={4} />
+                        <EllipseComponent sx={{ top: '200px' }} n={1} />
+                        <EllipseComponent sx={{ bottom: '0' }} n={2} />
+                        <EllipseComponent sx={{ top: '0' }} n={3} />
                         <Box sx={{
-                            margin: 5
+                            margin: 5,
+                            zIndex: 1000,
+                            position: 'absolute',
+                            top: 0
                         }}>
                             <Typography variant="h4" color="secondary.main" sx={{ pb: 4, fontWeight: 500 }}>
                                 Registra tu negocio
