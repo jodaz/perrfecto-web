c85eb932131e6ba7244519df27138459e2a9cd0b
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-02 20:04:17 -0400

    fix photo

--- src/utils/getUserPhoto.js
+++ src/utils/getUserPhoto.js
@@ -3,11 +3,15 @@ import vars from "../vars"
 const filePreviewOrigin = filepath => (`${vars.source}/${filepath}`)
 
 const getUserPhoto = filepath => {
-    // if (filepath.includes('googleusercontent')) {
-        return filepath
-    // } else {
-    //     return filePreviewOrigin(filepath)
-    // }
+    if (filepath) {
+        if (filepath.includes('googleusercontent')) {
+            return filepath
+        } else {
+            return filePreviewOrigin(filepath)
+        }
+    }
+
+    return null;
 }
 
 export default getUserPhoto;
