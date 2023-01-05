af9bc654ba0b480d03ae66dc596798545778168b
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-05 19:32:40 -0400

    close guest dialog on landing mount

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import Box from '@mui/material/Box'
 import BackgroundDogs from '../../assets/images/background-dogs.png'
 import useMediaQuery from '@mui/material/useMediaQuery';
@@ -6,11 +7,17 @@ import LinkBehavior from '../../components/LinkBehavior';
 import { Outlet, useNavigate } from 'react-router-dom'
 import getSearchParams from '../../utils/getSearchParams';
 import DeletedAccount from '../../components/Modals/DeletedAccount';
+import { closeGuestWarning, useGuest } from '../../context/GuestContext';
 
 const Landing = ({ location }) => {
+    const { dispatch: guestDispatch } = useGuest()
     const navigate = useNavigate();
     const openDeleteModal = getSearchParams(location, 'delete')
     const matches = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+
+    React.useEffect(() => {
+        closeGuestWarning(guestDispatch);
+    }, [])
 
     return (
         <Box sx={{
