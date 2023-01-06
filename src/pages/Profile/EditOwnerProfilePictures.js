a32cf928d8fb0e0585361539fabc3e1031567d62
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-06 12:01:11 -0400

    save work

--- src/pages/Profile/EditOwnerProfilePictures.js
+++ src/pages/Profile/EditOwnerProfilePictures.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import SettingsLayout from '../../layouts/SettingsLayout';
 import { useForm } from 'react-hook-form';
@@ -11,20 +12,27 @@ import formDataHandler from '../../utils/formDataHandler';
 const EditOwnerProfilePictures = () => {
     const [error, setError] = React.useState(null);
     const { state: { user }, dispatch } = useAuth();
-    const { handleSubmit, control, watch } = useForm();
+    const { control, handleSubmit, isSubmitting } = useForm();
 
-    const onSubmit = async (data) => {
+    const onSubmit = async ({ files }) => {
+        let formData = new FormData();
+
         try {
-            const parsedData = {
-                personalPhotos: data.files
-            }
+            if (files.length) {
+                for (let i = 0; i < files.length; i++) {
+                    console.log(files[i][0])
+                    // formData.append('files', files[i]);
+                }
 
-            const formData = await formDataHandler(parsedData, 'personalPhotos')
+                console.log(formData)
 
-            const res = await fileProvider.put(`/api/user/personal-photos`, formData)
+                // const formData = await formDataHandler(formData, 'personalPhotos')
 
-            if (res.status >= 200 && res.status < 300) {
-                renewToken(dispatch, user)
+                // const res = await fileProvider.put(`/api/user/personal-photos`, formData)
+
+                // if (res.status >= 200 && res.status < 300) {
+                //     renewToken(dispatch, user)
+                // }
             }
         } catch (error) {
             setError('Ha ocurrido un error inesperado.')
@@ -43,12 +51,6 @@ const EditOwnerProfilePictures = () => {
         }
     }
 
-    React.useEffect(() => {
-        const subscription = watch(handleSubmit(onSubmit))
-
-        return () => subscription.unsubscribe();
-    }, [handleSubmit, watch])
-
     return (
         <SettingsLayout title='Fotos personales'>
             <Box sx={{
@@ -56,27 +58,41 @@ const EditOwnerProfilePictures = () => {
                 display: 'flex',
                 flex: 1,
                 flexDirection: 'column',
-            }}>
-                <Typography textAlign="center" color="text.tertiary" sx={{ p: 2 }}>
-                    Estas fotos serán visibles para todos los usuarios.
-                </Typography>
-                <Box sx={{
-                    display: 'flex'
-                }}>
-                    <Box sx={{ p: 2, display: 'flex' }}>
-                        <PhotoInput
-                            name="files"
-                            control={control}
-                            handleDelete={deletePhoto}
-                        />
+                justifyContent: 'space-between'
+            }} component="form" onSubmit={handleSubmit(onSubmit)}>
+                <Box>
+                    <Typography textAlign="center" color="text.tertiary" sx={{ p: 2 }}>
+                        Estas fotos serán visibles para todos los usuarios.
+                    </Typography>
+                    <Box sx={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                        <Box sx={{ p: 2, display: 'flex' }}>
+                            <PhotoInput
+                                name="files[1]"
+                                control={control}
+                                handleDelete={deletePhoto}
+                            />
+                        </Box>
+                        <Box sx={{ p: 2, display: 'flex' }}>
+                            <PhotoInput
+                                name="files[2]"
+                                control={control}
+                                handleDelete={deletePhoto}
+                            />
+                        </Box>
                     </Box>
-                    <Box sx={{ p: 2, display: 'flex' }}>
-                        <PhotoInput
-                            name="files"
-                            control={control}
-                            handleDelete={deletePhoto}
-                        />
-                    </Box>
+                </Box>
+                <Box sx={{ p: 2 }}>
+                    <Button
+                        variant="contained"
+                        type="submit"
+                        fullWidth
+                        disabled={isSubmitting}
+                    >
+                        Guardar
+                    </Button>
                 </Box>
             </Box>
         </SettingsLayout>
