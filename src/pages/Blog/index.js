9c8bc403815138ae4e16d612f560d5dcfa3fcf91
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-07 22:40:15 -0400

    create post card and featured posts component

--- src/pages/Blog/index.js
+++ src/pages/Blog/index.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import FeaturedPosts from './FeaturedPosts';
 
 export default function Blog() {
     return (
         <Box sx={{ display: 'flex' }}>
-            {/* Aqui va el contenido del Blog */}
+            <Box sx={{ p: 2 }}>
+                <FeaturedPosts />
+            </Box>
         </Box>
     );
 }
