0c9eddce38b2833adae3deb0d031de1e48372568
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-09 23:51:34 -0400

    fix bug

--- src/pages/Market/index.js
+++ src/pages/Market/index.js
@@ -1,7 +1,28 @@
 import * as React from 'react';
+import Box from '@mui/material/Box'
+import FeaturedBusinessCard from '../Businesses/FeaturedBusinessCard';
+
+const featuredBusinesses = [
+    {
+        title: 'Petshop',
+        image: '/images/samples/sad-pupi.png',
+    },
+    {
+        title: 'Accesorios para perros',
+        image: '/images/samples/sad-pupi.png',
+    },
+    {
+        title: 'Busco un hogar',
+        image: '/images/samples/sad-pupi.png',
+    }
+]
 
 const Marketplace = () => {
-    return <></>
+    return (
+        <Box sx={{ display: 'flex' }}>
+            {featuredBusinesses.map(item => <FeaturedBusinessCard {...item} />)}
+        </Box>
+    )
 }
 
 export default Marketplace
