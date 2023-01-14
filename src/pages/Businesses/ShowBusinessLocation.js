2a5b8e862dc4a15f6e8b9a6ea7e2c692f9a83c7e
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-14 00:37:16 -0400

    complete show business location

---
+++ src/pages/Businesses/ShowBusinessLocation.js
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import SettingsLayout from '../../layouts/SettingsLayout';
+import {
+    MapContainer,
+    TileLayer,
+    Popup,
+    Marker
+} from 'react-leaflet'
+
+const ShowBusinessLocation = ({
+    business_name,
+    lat,
+    leng,
+    business_dir,
+    province,
+    city,
+    close
+}) => (
+    <SettingsLayout
+        title={business_name}
+        handleGoBack={close}
+    >
+        <Box sx={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+        }}>
+            <Box p={2}>
+                <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    fontWeight={500}
+                    gutterBottom
+                >
+                    Dirección
+                </Typography>
+                <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    fontWeight={500}
+                    gutterBottom
+                >
+                    {business_dir}, {province}, {city}
+                </Typography>
+            </Box>
+            <Box p={2}>
+                <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    fontWeight={500}
+                    gutterBottom
+                >
+                    Ubicar dentro de mapa
+                </Typography>
+                <MapContainer center={{ lat: lat, lng: leng }} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={{ lat: lat, lng: leng }}>
+                        <Popup minWidth={90} />
+                    </Marker>
+                </MapContainer>
+            </Box>
+        </Box>
+    </SettingsLayout>
+)
+
+export default ShowBusinessLocation
--- src/pages/Market/ShowMarket.js
+++ src/pages/Market/ShowMarket.js
@@ -9,137 +9,161 @@ import IconButton from '@mui/material/IconButton';
 import { Phone, MapPin, ChevronLeft, ArrowRight } from 'lucide-react'
 import getUserPhoto from '../../utils/getUserPhoto';
 import PhotoGallery from '../../components/Modals/ShowCard/PhotoGallery';
+import ShowBusinessLocation from '../Businesses/ShowBusinessLocation';
 
 const getImages = arrImages => arrImages.map(image => getUserPhoto(image));
 
-const ShowMarket = ({
-    phone,
-    facebook,
-    instagram,
-    website,
-    AnnMultimedia,
-    business_name,
-    province,
-    city,
-    description,
-    User,
-    close
-}) => (
-    <Slide direction="left" in={true} mountOnEnter unmountOnExit>
-        <Box sx={{
-            height: '100%',
-            width: '100%'
-        }}>
+const ShowMarket = ({ close, ...restData }) => {
+    const {
+        facebook,
+        instagram,
+        website,
+        AnnMultimedia,
+        business_name,
+        province,
+        city,
+        description,
+        User
+    } = restData
+    const [selectedItem, setSelectedItem] = React.useState(null);
+    const [showBusinessLocation, setShowBusinessLocation] = React.useState(false)
+
+    const handleOpenShowBusinessLocation = async (data) => {
+        setSelectedItem(data);
+        setShowBusinessLocation(true);
+    }
+
+    const handleCloseShowBusinessLocation = () => {
+        setShowBusinessLocation(false)
+    }
+
+    if (showBusinessLocation) {
+        return (
+            <ShowBusinessLocation
+                close={handleCloseShowBusinessLocation}
+                {...selectedItem}
+            />
+        )
+    }
+
+    return (
+        <Slide direction="left" in={true} mountOnEnter unmountOnExit>
             <Box sx={{
-                flex: 1,
-                height: 300,
-                width: '100%',
-                position: 'relative'
+                height: '100%',
+                width: '100%'
             }}>
                 <Box sx={{
-                    position: 'absolute',
-                    zIndex: 100,
-                    bgcolor: 'rgba(0, 0, 0, 0.36)',
-                    borderRadius: '100px',
-                    left: 20,
-                    top: 20
+                    flex: 1,
+                    height: 300,
+                    width: '100%',
+                    position: 'relative'
                 }}>
-                    <IconButton onClick={close}>
-                        <ChevronLeft color="#fff" />
-                    </IconButton>
+                    <Box sx={{
+                        position: 'absolute',
+                        zIndex: 100,
+                        bgcolor: 'rgba(0, 0, 0, 0.36)',
+                        borderRadius: '100px',
+                        left: 20,
+                        top: 20
+                    }}>
+                        <IconButton onClick={close}>
+                            <ChevronLeft color="#fff" />
+                        </IconButton>
+                    </Box>
+                    <PhotoGallery images={getImages(AnnMultimedia.map(item => item.name))} />
                 </Box>
-                <PhotoGallery images={getImages(AnnMultimedia.map(item => item.name))} />
-            </Box>
-            <Box sx={{
-                borderRadius: '24px 24px 0px 0px',
-                display: 'flex',
-                flexDirection: 'column',
-                borderTopLeftRadius: '16px',
-                borderTopRightRadius: '16px',
-            }}>
-                <Stack
-                    orientation='vertical'
-                    spacing={1}
-                    sx={{ p: 2 }}
-                >
-                    <Typography
-                        variant="h5"
-                        color="text.primary"
-                        fontWeight={500}
+                <Box sx={{
+                    borderRadius: '24px 24px 0px 0px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    borderTopLeftRadius: '16px',
+                    borderTopRightRadius: '16px',
+                }}>
+                    <Stack
+                        orientation='vertical'
+                        spacing={1}
+                        sx={{ p: 2 }}
                     >
-                        {business_name}
-                    </Typography>
-                    <Button
-                        color="info"
-                        sx={{
-                            padding: 0,
-                            margin: 0,
-                            justifyContent: 'start'
-                        }}
-                    >
-                        <MapPin size={18} /> {city}, {province}
-                    </Button>
-                    <Typography
-                        variant="subtitle1"
-                        color="info.main"
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center'
-                        }}
-                    >
-                        <Phone size={18} /><Box mr='10px' />  +{User.code_phone} {User.phone}
-                    </Typography>
-                    <Typography
-                        variant="subtitle1"
-                        color="text.secondary"
-                    >
-                        {description}
-                    </Typography>
-                </Stack>
-                <Stack
-                    orientation='vertical'
-                    spacing={1}
-                    sx={{ p: 2, width: 'fit-content' }}
-                >
-                    {website && (
-                        <Button
-                            color="primary"
-                            variant="contained"
-                            target='_blank'
-                            href={`//${website}`}
-                            component={Link}
+                        <Typography
+                            variant="h5"
+                            color="text.primary"
+                            fontWeight={500}
                         >
-                            Ir a la página
-                            <ArrowRight />
-                        </Button>
-                    )}
-                    {facebook && (
+                            {business_name}
+                        </Typography>
                         <Button
                             color="info"
-                            variant="contained"
-                            target='_blank'
-                            href={`//${facebook}`}
-                            component={Link}
+                            sx={{
+                                padding: 0,
+                                margin: 0,
+                                justifyContent: 'start'
+                            }}
+                            onClick={() => handleOpenShowBusinessLocation(restData)}
                         >
-                            Ir a facebook
-                            <ArrowRight />
+                            <MapPin size={18} /> {city}, {province}
                         </Button>
-                    )}
-                    {instagram && (
-                        <Button
-                            color="success"
-                            variant="contained"
-                            target='_blank'
-                            href={`//${instagram}`}
+                        <Typography
+                            variant="subtitle1"
+                            color="info.main"
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center'
+                            }}
                         >
-                            Ir a instagram
-                            <ArrowRight />
-                        </Button>
-                    )}
-                </Stack>
+                            <Phone size={18} /><Box mr='10px' />  +{User.code_phone} {User.phone}
+                        </Typography>
+                        <Typography
+                            variant="subtitle1"
+                            color="text.secondary"
+                        >
+                            {description}
+                        </Typography>
+                    </Stack>
+                    <Stack
+                        orientation='vertical'
+                        spacing={1}
+                        sx={{ p: 2, width: 'fit-content' }}
+                    >
+                        {website && (
+                            <Button
+                                color="primary"
+                                variant="contained"
+                                target='_blank'
+                                href={`//${website}`}
+                                component={Link}
+                            >
+                                Ir a la página
+                                <ArrowRight />
+                            </Button>
+                        )}
+                        {facebook && (
+                            <Button
+                                color="info"
+                                variant="contained"
+                                target='_blank'
+                                href={`//${facebook}`}
+                                component={Link}
+                            >
+                                Ir a facebook
+                                <ArrowRight />
+                            </Button>
+                        )}
+                        {instagram && (
+                            <Button
+                                color="success"
+                                variant="contained"
+                                target='_blank'
+                                href={`//${instagram}`}
+                            >
+                                Ir a instagram
+                                <ArrowRight />
+                            </Button>
+                        )}
+                    </Stack>
+                </Box>
             </Box>
-        </Box>
-    </Slide>
-)
+        </Slide>
+    )
+}
 
 export default ShowMarket
