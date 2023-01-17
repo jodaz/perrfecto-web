28ca428fa6c19036a7daa3e8b56ffa953f5e5103
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-17 12:10:56 -0400

    add "my blogs" button

--- src/pages/Blog/RecentPosts.js
+++ src/pages/Blog/RecentPosts.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import LinkBehavior from '../../components/LinkBehavior'
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import RecentPostCard from './RecentPostCard';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Scrollbar, Navigation } from 'swiper';
@@ -52,14 +54,33 @@ const posts = [
 
 const RecentPosts = ({ openPost }) =>  (
     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <Typography
-            variant="subtitle1"
-            fontWeight={500}
-            fontSize='1.2rem'
-            mb={2}
-        >
-            Recientes
-        </Typography>
+        <Box sx={{
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            mb: 2
+        }}>
+            <Typography
+                variant="h5"
+                fontWeight={500}
+            >
+                Recientes
+            </Typography>
+            <Button
+                component={LinkBehavior}
+                variant="outlined"
+                to='/blogs/me'
+                size='small'
+                sx={{
+                    fontSize: '0.9rem',
+                    padding: '0.4rem 0.7rem',
+                    fontWeight: 500
+                }}
+            >
+                Ver mis blogs
+            </Button>
+        </Box>
         <SwiperStyled
             slidesPerView={1}
             scrollbar={{
