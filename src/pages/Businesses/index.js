c7f3e597edcab02a1566c57a7804593692cf5182
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-17 00:04:59 -0400

    integrate show business

--- src/pages/Businesses/index.js
+++ src/pages/Businesses/index.js
@@ -7,31 +7,10 @@ import IconButtonWithTitle from '../../components/IconButtonWithTitle';
 import SettingsLayout from '../../layouts/SettingsLayout';
 import BusinessCard from './BusinessCard';
 import ShowBusiness from './ShowBusiness';
-
-const showBusiness = {
-    name: 'Petshop',
-    province: 'España',
-    city: 'Sevilla',
-    description: 'Hola, somos un Petshop. En nuestro negocio se encuentran diferentes productos para tu mascota. Apostamos siempre por la calidad del servicio que brindamos. Para más información click en el botón',
-    images: JSON.stringify([
-        '/images/samples/sad-pupi.png',
-        '/images/samples/sad-pupi.png',
-        '/images/samples/sad-pupi.png',
-        '/images/samples/sad-pupi.png'
-    ]),
-    phone: '+58 0426 1843880',
-    facebook: 'https://facebook.com',
-    instagram: 'https://instagram.com',
-    website: 'https://facebook.com'
-}
-
-const businesses = [
-    showBusiness,
-    showBusiness,
-    showBusiness
-]
+import { useAuth } from '../../context/AuthContext';
 
 const Businesses = () => {
+    const { state: { user } } = useAuth();
     const [selectedItem, setSelectedItem] = React.useState(null);
     const [showBusiness, setShowBusiness] = React.useState(false)
 
@@ -77,13 +56,16 @@ const Businesses = () => {
                         title='Crear negocio'
                     />
                 </Box>
-                {/* <Stack
+                <Stack
                     p={2}
                     orientation='vertical'
                     spacing={2}
                 >
-                    {businesses.map(item => <BusinessCard {...item} handleSelect={handleOpenShowBusiness} />)}
-                </Stack> */}
+                    <BusinessCard
+                        {...user.publication}
+                        handleSelect={handleOpenShowBusiness}
+                    />
+                </Stack>
             </Box>
         </SettingsLayout>
     )
