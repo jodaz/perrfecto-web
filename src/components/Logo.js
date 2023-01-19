d1e0880ea774e4c9b436ed09496db97619e328fc
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-18 23:02:53 -0400

    add logic for dark version of logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,16 +1,8 @@
 import * as React from 'react'
-import Box from '@mui/material/Box'
 import { ReactComponent as LogoSvg } from '../assets/icons/Logo.svg'
-import Typography from '@mui/material/Typography';
 
-const Logo = ({
-    color = 'secondary.main',
-    height = '30px',
-    width = '30px'
-}) => (
-    <Box sx={{ display: 'flex', alignItems: 'center', color: '#fff', fontWeight: 900, fontSize: '1.15rem' }}>
-        <LogoSvg />
-    </Box>
+const Logo = ({ dark }) => (
+    dark ? <LogoSvg /> : <LogoSvg />
 );
 
 export default Logo;
