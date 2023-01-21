ad7211b2cb0bae738fcb61056dc2aa5b0c9e51a1
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-21 14:16:15 -0400

    integrate endpoints for listing posts

--- src/pages/Blog/PostCard.js
+++ src/pages/Blog/PostCard.js
@@ -10,99 +10,96 @@ import { es } from 'date-fns/locale'
 import getUserPhoto from '../../utils/getUserPhoto';
 import LinkBehavior from '../../components/LinkBehavior';
 
-const PostCard = ({ handleClick, ...data }) => {
-    const {
-        title,
-        BlogMultimedia,
-        createdAt,
-        commentsCount,
-        likesCount
-    } = data;
-
-    return (
-        <Card
-            variant="outlined"
+const PostCard = ({
+    id,
+    title,
+    BlogMultimedia,
+    createdAt,
+    commentsCount,
+    likesCount
+}) => (
+    <Card
+        variant="outlined"
+        sx={{
+            display: 'flex',
+            border: 'none',
+            textDecoration: 'none',
+            color: 'unset',
+            flexDirection: { xs: 'column', sm: 'row' },
+            transition: '0.3s',
+            cursor: 'pointer',
+            '&: hover': {
+                opacity: 0.75
+            }
+        }}
+        component={LinkBehavior}
+        to={`/blogs/${id}`}
+    >
+        <CardMedia
+            component="img"
+            width="130px"
+            height="140px"
+            alt="post_cover"
+            src={getUserPhoto(BlogMultimedia[0].name)}
             sx={{
-                display: 'flex',
-                border: 'none',
-                textDecoration: 'none',
-                color: 'unset',
-                flexDirection: { xs: 'column', sm: 'row' },
-                transition: '0.3s',
-                cursor: 'pointer',
-                '&: hover': {
-                    opacity: 0.75
-                }
+                borderRadius: 2,
+                minWidth: '130px',
+                minHeight: '130px',
+                maxWidth: '130px',
+                maxHeight: '130px'
             }}
-            component={LinkBehavior}
-            to={`/blogs/${data.id}`}
-        >
-            <CardMedia
-                component="img"
-                width="130px"
-                height="140px"
-                alt="post_cover"
-                src={getUserPhoto(BlogMultimedia[0].name)}
-                sx={{
-                    borderRadius: 2,
-                    minWidth: '130px',
-                    minHeight: '130px',
-                    maxWidth: '130px',
-                    maxHeight: '130px'
-                }}
-            />
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-                px: { xs: 0, sm: 2 }
-            }}>
-                <Box>
-                    <Typography
-                        variant="body1"
-                        color="text.secondary"
-                        fontWeight={700}
-                        sx={{
-                            textAlign: { xs: 'center', sm: 'start' },
-                            mt: { xs: 1.5, sm: 0 },
-                        }}
-                    >
-                        {title}
-                    </Typography>
-                    <Typography
-                        component="div"
-                        variant="caption"
-                        color="text.tertiary"
-                        fontWeight={500}
-                        sx={{ textAlign: { xm: 'center', sm: 'start' }, textTransform: 'capitalize' }}
-                    >
-                        {format(new Date(createdAt), 'MMMM d, y', { locale: es })}
-                    </Typography>
-                </Box>
-                <Stack
-                    direction="row"
-                    spacing={1}
+        />
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            px: { xs: 0, sm: 2 }
+        }}>
+            <Box>
+                <Typography
+                    variant="body1"
+                    color="text.secondary"
+                    fontWeight={700}
                     sx={{
-                        justifyContent: { xs: 'space-between', sm: 'flex-start' },
-                        alignItems: 'center'
+                        textAlign: { xs: 'center', sm: 'start' },
+                        mt: { xs: 1.5, sm: 0 },
                     }}
                 >
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <ThumbsUp color="#5E5E5E" />
-                        <Typography variant="body2" ml={1} color="#5E5E5E">
-                            {likesCount}
-                        </Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <MessageSquare color="#5E5E5E" />
-                        <Typography variant="body2" ml={1} color="#5E5E5E">
-                            {commentsCount}
-                        </Typography>
-                    </Box>
-                </Stack>
+                    {title}
+                </Typography>
+                <Typography
+                    component="div"
+                    variant="caption"
+                    color="text.tertiary"
+                    fontWeight={500}
+                    sx={{ textAlign: { xm: 'center', sm: 'start' }, textTransform: 'capitalize' }}
+                >
+                    {format(new Date(createdAt), 'MMMM d, y', { locale: es })}
+                </Typography>
             </Box>
-        </Card>
-    );
-}
+            <Stack
+                direction="row"
+                spacing={1}
+                sx={{
+                    justifyContent: { xs: 'space-between', sm: 'flex-start' },
+                    alignItems: 'center'
+                }}
+            >
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <ThumbsUp color="#5E5E5E" />
+                    <Typography variant="body2" ml={1} color="#5E5E5E">
+                        {likesCount}
+                    </Typography>
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <MessageSquare color="#5E5E5E" />
+                    <Typography variant="body2" ml={1} color="#5E5E5E">
+                        {commentsCount}
+                    </Typography>
+                </Box>
+            </Stack>
+        </Box>
+    </Card>
+);
 
 export default PostCard
--- src/pages/Blog/RecentPostCard.js
+++ src/pages/Blog/RecentPostCard.js
@@ -5,96 +5,95 @@ import Box from '@mui/material/Box';
 import CardMedia from '@mui/material/CardMedia';
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale'
+import getUserPhoto from '../../utils/getUserPhoto';
+import LinkBehavior from '../../components/LinkBehavior';
 
-const RecentPostCard = ({ handleClick, ...data }) => {
-    const {
-        title,
-        image,
-        published_at,
-        name,
-        lastName,
-        img_profile
-    } = data
-
-    return (
-        <Card
-            variant="outlined"
+const RecentPostCard = ({
+    id,
+    title,
+    BlogMultimedia,
+    createdAt
+}) => (
+    <Card
+        variant="outlined"
+        sx={{
+            display: 'flex',
+            textDecoration: 'none',
+            color: 'unset',
+            border: 'none',
+            flexDirection: 'column',
+            transition: '0.3s',
+            cursor: 'pointer',
+            '&: hover': {
+                opacity: 0.75
+            }
+        }}
+        component={LinkBehavior}
+        to={`/blogs/${id}`}
+    >
+        <CardMedia
+            component="img"
+            width="130"
+            height="140"
+            alt='blog_post.png'
+            src={getUserPhoto(BlogMultimedia[0].name)}
             sx={{
-                display: 'flex',
-                border: 'none',
-                flexDirection: 'column',
-                transition: '0.3s',
-                cursor: 'pointer',
-                '&: hover': {
-                    opacity: 0.75
-                }
+                borderRadius: 4,
             }}
-            onClick={() => handleClick(data)}
-        >
-            <CardMedia
-                component="img"
-                width="130"
-                height="140"
-                alt='blog_post.png'
-                src={image}
-                sx={{
-                    borderRadius: 4,
-                }}
-            />
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-                mt: 1
-            }}>
-                <Box>
-                    <Typography
-                        variant="body1"
-                        color="text.secondary"
-                        fontWeight={700}
-                        sx={{
-                            textAlign: { xs: 'center', sm: 'start' },
-                            mt: { xs: 1.5, sm: 0 },
-                        }}
-                    >
-                        {title}
-                    </Typography>
-                    <Box sx={{ display: 'flex', mt: 1}}>
-                        <Box sx={{ display: 'flex', justifyContent: 'start', flex: 1 }}>
-                            <Box
-                                component="img"
-                                alt='blog_post.png'
-                                src={img_profile}
-                                sx={{
-                                    maxWidth: 22,
-                                    maxHeight: 22,
-                                    borderRadius: 1,
-                                    mr: 1
-                                }}
-                            />
-                            <Typography
-                                component="div"
-                                variant="caption"
-                                color="text.secondary"
-                                fontWeight={500}
-                            >
-                                {`${name} ${lastName}`}
-                            </Typography>
-                        </Box>
+        />
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            mt: 1
+        }}>
+            <Box>
+                <Typography
+                    variant="body1"
+                    color="text.secondary"
+                    fontWeight={700}
+                    sx={{
+                        textAlign: { xs: 'center', sm: 'start' },
+                        mt: { xs: 1.5, sm: 0 },
+                    }}
+                >
+                    {title}
+                </Typography>
+                <Box sx={{ display: 'flex', mt: 1}}>
+                    <Box sx={{ display: 'flex', justifyContent: 'start', flex: 1 }}>
+                        {/* <Box
+                            component="img"
+                            alt='blog_post.png'
+                            src={img_profile}
+                            sx={{
+                                maxWidth: 22,
+                                maxHeight: 22,
+                                borderRadius: 1,
+                                mr: 1
+                            }}
+                        />
                         <Typography
                             component="div"
                             variant="caption"
-                            color="text.tertiary"
+                            color="text.secondary"
                             fontWeight={500}
-                            sx={{ textAlign: { xm: 'center', sm: 'start' }, textTransform: 'capitalize' }}
                         >
-                            {format(published_at, 'MMMM d, y', { locale: es })}
-                        </Typography>
+                            {`${name} ${lastName}`}
+                        </Typography> */}
                     </Box>
+                    <Typography
+                        component="div"
+                        variant="caption"
+                        color="text.tertiary"
+                        fontWeight={500}
+                        sx={{ textAlign: { xm: 'center', sm: 'start' }, textTransform: 'capitalize' }}
+                    >
+                        {format(new Date(createdAt), 'MMMM d, y', { locale: es })}
+                    </Typography>
                 </Box>
             </Box>
-        </Card>
-    );
-}
+        </Box>
+    </Card>
+);
 
 export default RecentPostCard
