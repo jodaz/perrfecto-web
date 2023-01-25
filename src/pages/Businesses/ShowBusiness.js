7a3f22e76bd0403f289507b767a983dc231f9a32
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-25 12:53:42 -0400

    add function to feature business

--- src/pages/Businesses/ShowBusiness.js
+++ src/pages/Businesses/ShowBusiness.js
@@ -3,19 +3,19 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import { Phone, MapPin, Trash2, Edit, ArrowRight } from 'lucide-react'
+import { Phone, MapPin, Trash2, Edit, ArrowRight, Star } from 'lucide-react'
 import getUserPhoto from '../../utils/getUserPhoto';
 import PhotoGallery from '../../components/Modals/ShowCard/PhotoGallery';
 import Menu from '../../components/Menu';
 import SettingsLayout from '../../layouts/SettingsLayout';
-import DeleteBusiness from '../../components/Modals/DeleteBusiness';
+import FeatureBusiness from '../../components/Modals/FeatureBusiness';
 import ShowBusinessLocation from './ShowBusinessLocation';
 import LinkBehavior from '../../components/LinkBehavior';
 
 const getImages = arrImages => arrImages.map(image => getUserPhoto(image));
 
 const ShowBusiness = ({ close, ...item }) => {
-    const [openDeleteModal, setOpenDeleteModal] = React.useState(false)
+    const [featureBusiness, setFeatureBusiness] = React.useState(false)
     const {
         facebook,
         instagram,
@@ -38,16 +38,22 @@ const ShowBusiness = ({ close, ...item }) => {
         setShowBusinessLocation(false)
     }
 
-    const handleOpenDeleteModal = async () => {
-        setOpenDeleteModal(true);
-    }
-
-    const handleCloseDeleteModal = () => {
-        setOpenDeleteModal(false)
-    }
+   const toggleFeatureBusiness = () => setFeatureBusiness(!featureBusiness)
 
     const renderMenu = () => (
         <Menu>
+            <Box onClick={toggleFeatureBusiness}
+                sx={{
+                display: 'flex',
+                alignItems: 'center',
+                color: 'unset',
+                textDecoration: 'none',
+            }}>
+                <Star />
+                <Box sx={{ paddingLeft: '0.5rem' }}>
+                    Destacar negocio
+                </Box>
+            </Box>
             <Box component={LinkBehavior}
                 sx={{
                 display: 'flex',
@@ -58,15 +64,6 @@ const ShowBusiness = ({ close, ...item }) => {
                 <Edit />
                 <Box sx={{ paddingLeft: '0.5rem' }}>
                     Editar negocio
-                </Box>
-            </Box>
-            <Box sx={{
-                display: 'flex',
-                alignItems: 'center'
-            }} onClick={handleOpenDeleteModal}>
-                <Trash2 />
-                <Box sx={{ paddingLeft: '0.5rem' }}>
-                    Eliminar negocio
                 </Box>
             </Box>
         </Menu>
@@ -189,10 +186,11 @@ const ShowBusiness = ({ close, ...item }) => {
                         )}
                     </Stack>
                 </Box>
-                <DeleteBusiness
-                    open={openDeleteModal}
-                    handleClose={handleCloseDeleteModal}
+                <FeatureBusiness
+                    open={featureBusiness}
+                    handleClose={toggleFeatureBusiness}
                     closeBusiness={close}
+                    item={item}
                 />
             </Box>
         </SettingsLayout>
