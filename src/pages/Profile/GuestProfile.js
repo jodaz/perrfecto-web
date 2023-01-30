214ec8023133e5215de542dc458d9342eb6dd3ee
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-30 12:35:10 -0400

    remove settings layout

--- src/pages/Profile/GuestProfile.js
+++ src/pages/Profile/GuestProfile.js
@@ -3,34 +3,31 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { logout, useAuth } from '../../context/AuthContext';
 import LinkBehavior from '../../components/LinkBehavior';
-import SettingsLayout from '../../layouts/SettingsLayout'
 
 const GuestProfile = () => {
     const { dispatch } = useAuth();
 
     return (
-        <SettingsLayout title="Configuraciones">
-            <Box sx={{
-                height: '100%',
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'end'
-            }}>
-                <Box sx={{ p: 2 }}>
-                    <Button
-                        variant="outlined"
-                        color="error"
-                        fullWidth
-                        onClick={() => logout(dispatch)}
-                        component={LinkBehavior}
-                        to='/'
-                    >
-                        Cerrar sesión
-                    </Button>
-                </Box>
+        <Box sx={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'end'
+        }}>
+            <Box sx={{ p: 2 }}>
+                <Button
+                    variant="outlined"
+                    color="error"
+                    fullWidth
+                    onClick={() => logout(dispatch)}
+                    component={LinkBehavior}
+                    to='/'
+                >
+                    Cerrar sesión
+                </Button>
             </Box>
-        </SettingsLayout>
+        </Box>
     );
 }
 
