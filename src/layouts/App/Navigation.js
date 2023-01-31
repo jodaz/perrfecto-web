deb8fab209c3a7fd0e1e98cd91cd23559c5171cc
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-31 01:25:24 -0400

    add logout for guest user

--- src/layouts/App/Navigation.js
+++ src/layouts/App/Navigation.js
@@ -5,19 +5,13 @@ import { useLocation } from 'react-router-dom';
 // Icons
 import { ReactComponent as StoreIcon } from '../../assets/icons/Store.svg'
 import { ReactComponent as StoreActiveIcon } from '../../assets/icons/StoreActive.svg'
-import { Newspaper } from 'lucide-react';
+import { LogOut, Newspaper } from 'lucide-react';
 import { MessageCircle } from 'lucide-react';
 import { Home } from 'lucide-react';
 import { User } from 'lucide-react';
 import { Dog } from 'lucide-react';
 import PrivateRoute from '../../components/PrivateRoute';
-
-const profileIcon = [{
-    label: 'Perfil',
-    icon: <Dog color='#ccc' />,
-    active: <Dog color='#A167C9' />,
-    route: '/profile'
-}]
+import { logout, useAuth } from '../../context/AuthContext';
 
 const smallScreenOnly = [
     {
@@ -44,6 +38,16 @@ const generalLinks = [
     }
 ];
 
+const guestLinks = handleClick => ([
+    ...generalLinks,
+    {
+        label: 'Salir',
+        icon: <LogOut color='red' onClick={handleClick} />,
+        active: <LogOut color='red' onClick={handleClick} />,
+        route: '/'
+    }
+])
+
 const onlyUserLinks = [
     ...generalLinks,
     {
@@ -51,6 +55,12 @@ const onlyUserLinks = [
         icon: <MessageCircle color='#ccc' />,
         active: <MessageCircle color='#A167C9' />,
         route: '/chat'
+    },
+    {
+        label: 'Perfil',
+        icon: <Dog color='#ccc' />,
+        active: <Dog color='#A167C9' />,
+        route: '/profile'
     }
 ]
 
@@ -77,10 +87,11 @@ const onlyBusiness = [
 
 const Navigation = ({ isSmall }) => {
     const location = useLocation();
+    const { dispatch } = useAuth()
 
     const renderLinks = icons => icons.map((icon, i) => (
         <Tooltip key={i} title={icon.label}>
-            <IconButton component={LinkBehavior} to={icon.route} sx={{ margin: '0 0.75rem'}}>
+            <IconButton component={LinkBehavior} to={icon.route} onClick={icon.onClick} sx={{ margin: '0 0.75rem'}}>
                 {
                     React.cloneElement(
                         (location.pathname.startsWith(icon.route)) ? icon.active : icon.icon,
@@ -101,12 +112,10 @@ const Navigation = ({ isSmall }) => {
         }}>
             {isSmall && renderLinks(smallScreenOnly)}
             <PrivateRoute authorize='guest' unauthorized={null}>
-                {renderLinks(generalLinks)}
-                {renderLinks(profileIcon)}
+                {renderLinks(guestLinks(() => logout(dispatch)))}
             </PrivateRoute>
             <PrivateRoute authorize='user' unauthorized={null}>
                 {renderLinks(onlyUserLinks)}
-                {renderLinks(profileIcon)}
             </PrivateRoute>
             <PrivateRoute authorize='business' unauthorized={null}>
                 {renderLinks(onlyBusiness)}
