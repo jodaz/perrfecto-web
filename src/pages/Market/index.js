0cfa95a6bbe6d47dd5fe6c6f425fdd54f7057d44
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-01-31 23:44:58 -0400

    add padding to search box

--- src/pages/Market/index.js
+++ src/pages/Market/index.js
@@ -66,7 +66,9 @@ const Marketplace = () => {
                     <SlidersHorizontal onClick={() => toggleFilters(dispatch)}/>
                 </IconButton>
             </Box>
-            <SearchBox filter={filterFunction} />
+            <Box p={2}>
+                <SearchBox filter={filterFunction} />
+            </Box>
             {(!isLoaded) ?
             (
                 <>
