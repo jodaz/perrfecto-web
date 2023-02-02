9e4a9207bbbbb82b1ee30b5bc3f833e5970f249e
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-02-01 23:13:24 -0400

    integrate business endpoint in edit business

--- src/pages/Businesses/EditBusiness/index.js
+++ src/pages/Businesses/EditBusiness/index.js
@@ -4,22 +4,38 @@ import SettingsLayout from '../../../layouts/SettingsLayout';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../../../context/AuthContext';
 import { saveStep, useMultiStepForm } from '../../../context/MultiStepContext';
+import { apiProvider } from '../../../api';
+import useEffectOnce from '../../../utils/useEffectOnce';
 
 const EditBusiness = () => {
     const { state: { user } } = useAuth()
     const { dispatch } = useMultiStepForm()
 
-    React.useEffect(() => {
-        if (user.publication) {
-            const {
-                AnnMultimedia,
-                ...restPublication
-            } = user.publication;
+    const fetchBusiness = async () => {
+        try {
+            const res = await apiProvider.get(`/api/business-ann/business/${user.publication.id}`)
 
-            saveStep(dispatch, {
-                files: AnnMultimedia.map(item => item.name),
-                ...restPublication
-            })
+            if (res.status >= 200 && res.status < 300) {
+                const { data: { data } } = res;
+
+                const {
+                    AnnMultimedia,
+                    ...restData
+                } = data;
+
+                saveStep(dispatch, {
+                    files: AnnMultimedia.map(item => item.name),
+                    ...restData
+                })
+            }
+        } catch (error) {
+            console.log("error ", error)
+        }
+    }
+
+    useEffectOnce(() => {
+        if (user.publication) {
+            fetchBusiness()
         }
     }, [])
 
