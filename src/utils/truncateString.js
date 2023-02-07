c1b662449eb2bb2d1accffe9f5f3b2c112951c9d
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-02-06 20:19:01 -0400

    shotend names in recent post card

---
+++ src/utils/truncateString.js
@@ -0,0 +1,9 @@
+const truncateString = (str, num) => {
+    if (str.length > num) {
+        return `${str.slice(0, num).trimEnd()}...`;
+    }
+
+    return str;
+}
+
+export default truncateString
