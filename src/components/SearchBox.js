fb6e6e2ca2a474fc1a954dfbe957f97698319d2e
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-02-11 15:10:34 -0400

    add icon to remove searches from searchbox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,23 @@
 import * as React from 'react';
-import { InputAdornment, InputBase } from '@mui/material';
-import { Search } from 'lucide-react';
+import { IconButton, InputAdornment, InputBase } from '@mui/material';
+import { Search, X, XCircle } from 'lucide-react';
 
 const SearchBox = ({ filter }) => {
+    const [value, setValue] = React.useState('');
+
     const handleOnChange = (e) => {
-        filter({
-            search: e.currentTarget.value
-        })
+        setValue(e.currentTarget.value)
     }
+
+    const clearInput = () => setValue('')
+
+    React.useEffect(() => {
+        filter({ search: value })
+    }, [value])
 
     return (
         <InputBase
+            value={value}
             placeholder='Buscar'
             sx={{
                 borderRadius: '8px !important',
@@ -22,6 +29,15 @@ const SearchBox = ({ filter }) => {
                 <InputAdornment sx={{ marginRight: '0.5rem'}}>
                     <Search color="#A6A6A6" />
                 </InputAdornment>
+            }
+            endAdornment={
+                value && (
+                    <InputAdornment>
+                        <IconButton color="error" onClick={clearInput}>
+                            <XCircle size={18} />
+                        </IconButton>
+                    </InputAdornment>
+                )
             }
             onChange={handleOnChange}
         />
