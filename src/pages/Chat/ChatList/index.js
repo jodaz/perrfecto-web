98b933da2486449e8770c466b0ebd818902d201f
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-02-13 16:14:23 -0400

    fetch users

--- src/pages/Chat/ChatList/index.js
+++ src/pages/Chat/ChatList/index.js
@@ -5,70 +5,27 @@ import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import SearchBox from '../../../components/SearchBox';
 import UserMessageCard from './UserMessageCard';
-
-const messagesArray = [
-    {
-        id: 1,
-        message: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-        created_at: new Date(),
-        User: {
-            name: 'Eminem',
-            user_picture: '/images/samples/Pupi.png'
-        },
-        connected: true
-    },
-    {
-        id: 2,
-        message: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-        created_at: new Date( Date.now() - 1000 * 60 ), // 45 min
-        User: {
-            name: 'J',
-            lastName: 'Balvin',
-            user_picture: '/images/samples/Pupi.png'
-        },
-        connected: false
-    },
-    {
-        id: 3,
-        message: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-        created_at: new Date(2023, 1, 4, 10, 33, 1), // Unos dias
-        User: {
-            name: 'Kygo',
-            user_picture: '/images/samples/Pupi.png'
-        },
-        connected: true
-    },
-    {
-        id: 4,
-        message: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-        created_at: new Date(2023, 0, 28, 10, 33, 1), // Unos dias
-        User: {
-            name: 'Deadmau5',
-            user_picture: '/images/samples/Pupi.png'
-        },
-        connected: false
-    },
-    {
-        id: 5,
-        message: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-        created_at: new Date(2023, 0, 4, 20, 33, 1), // 2 sem
-        User: {
-            name: 'Don',
-            lastName: 'Diablo',
-            user_picture: '/images/samples/Pupi.png'
-        },
-        connected: true
-    },
-]
+import { apiProvider } from '../../../api';
+import useEffectOnce from '../../../utils/useEffectOnce';
 
 export default function ChatList() {
     const [messages, setMessages] = React.useState([null, null, null, null, null])
 
-    React.useEffect(() => {
-        setInterval(() => {
-            setMessages(messagesArray)
-        }, 5000);
-    }, [])
+    const fetchMessages = async () => {
+        try {
+            const res = await apiProvider.get('/api/chat/conversations')
+
+            if (res.status >= 200 && res.status < 300) {
+                const { data: { data } } = res;
+
+                setMessages(data)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffectOnce(() => { fetchMessages() }, [])
 
     return (
         <Box p={2}>
@@ -79,7 +36,7 @@ export default function ChatList() {
                 >
                     Mensajes
                 </Typography>
-                <SearchBox />
+                <SearchBox filter={data => console.log(data)} />
             </Stack>
             <Box mt={3} />
             <List sx={{ width: '100%' }}>
