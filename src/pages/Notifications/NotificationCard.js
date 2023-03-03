9ed4eba364987a0d209c76063fb449e4bd1a5dd5
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-03-03 15:48:11 -0400

    implement list notifications endpoint

--- src/pages/Notifications/NotificationCard.js
+++ src/pages/Notifications/NotificationCard.js
@@ -1,31 +1,44 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button'
-import { ReactComponent as StandardIcon } from '../../assets/icons/Standard.svg'
 
-const NotificationCard = ({ children }) => (
-    <Box sx={{
-        display: 'flex',
-        alignItems: 'center',
-        margin: '1rem 0'
-    }}>
-        <Box>
-            <StandardIcon />
-        </Box>
+const newMessage = ({ name_sender }) => `Has recibido un nuevo mensaje de ${name_sender}.`
+
+const NotificationCard = props => {
+    console.log(props)
+    const {
+        id,
+        img,
+        key,
+        link,
+        name_sender,
+        status,
+        createdAt
+    } = props
+
+    return (
         <Box sx={{
-            color: theme => theme.palette.text.secondary,
-            fontSize: '14px'
-        }}>
-            {children}
-        </Box>
-        <Box marginLeft='0.5rem'>
-            <Button variant="outlined" sx={{
-                fontSize: '14px',
-                padding: '0.3rem 0.4rem',
+            display: 'flex',
+            alignItems: 'center',
+            margin: '1rem 0'
+        }} key={id}>
+            <Box>
+            </Box>
+            <Box sx={{
+                color: theme => theme.palette.text.secondary,
+                fontSize: '14px'
             }}>
-                Renovar
-            </Button>
+                {(key == 'new_message') && newMessage(props)}
+            </Box>
+            <Box marginLeft='0.5rem'>
+                <Button variant="outlined" sx={{
+                    fontSize: '14px',
+                    padding: '0.3rem 0.4rem',
+                }}>
+                    Renovar
+                </Button>
+            </Box>
         </Box>
-    </Box>
-)
+    )
+}
 
 export default NotificationCard
