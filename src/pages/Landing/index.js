5e9a3739f14ddf1eb243a92bdd0b0d19119218b7
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-03-04 23:50:48 -0400

    craete register business

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -3,17 +3,16 @@ import { useNavigate } from 'react-router-dom'
 import getSearchParams from '../../utils/getSearchParams';
 import DeletedAccount from '../../components/Modals/DeletedAccount';
 import { closeGuestWarning, useGuest } from '../../context/GuestContext';
-import { useAuth } from '../../context/AuthContext'
 import Box from '@mui/material/Box'
 // Sections
 import Intro from './Intro';
 import DownloadAppSection from './DownloadAppSection';
+import RegisterBusinessCalling from './RegisterBusinessCalling';
 
 const Landing = ({ location }) => {
     const { dispatch: guestDispatch } = useGuest()
     const navigate = useNavigate();
     const openDeleteModal = getSearchParams(location, 'delete')
-    const { state: { isAuth }, dispatch: authDispatch } = useAuth();
 
     React.useEffect(() => {
         closeGuestWarning(guestDispatch);
@@ -25,6 +24,7 @@ const Landing = ({ location }) => {
             flexDirection: 'column'
         }}>
             <Intro />
+            <RegisterBusinessCalling />
             <DownloadAppSection />
             <DeletedAccount open={openDeleteModal} handleClose={() => navigate('/')} />
         </Box>
