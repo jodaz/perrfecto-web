5a9202726e4d99ac1608be0a9b06e69576d87302
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-03-19 21:53:21 -0400

    correct font size

--- src/components/ItemWithCheck.js
+++ src/components/ItemWithCheck.js
@@ -2,7 +2,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography'
 import { CheckCircle2 } from 'lucide-react';
 
-const ItemWithCheck = ({ children, color }) => (
+const ItemWithCheck = ({ children, color = '#FFD900' }) => (
     <Box sx={{
         display: 'flex',
         alignItems: 'start',
@@ -10,9 +10,17 @@ const ItemWithCheck = ({ children, color }) => (
         fontWeight: 400
     }}>
         <Box mr={1} color={color}>
-            <CheckCircle2 size={16} color={color} />
+            <CheckCircle2
+                size={18}
+                color={color}
+            />
         </Box>
-        <Typography variant="subtitle1" textAlign='left' gutterBottom>
+        <Typography
+            variant="subtitle1"
+            textAlign='left'
+            gutterBottom
+            fontSize='15px'
+        >
             {children}
         </Typography>
     </Box>
