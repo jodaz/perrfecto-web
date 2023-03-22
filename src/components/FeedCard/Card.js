fd34054074acbb2c2333c8f75aaebd64f3e8471a
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-03-22 14:02:42 -0400

    refactor checkout process

--- src/components/FeedCard/Card.js
+++ src/components/FeedCard/Card.js
@@ -3,7 +3,6 @@ import MuiCard from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
--- src/pages/packs/PaymentCheckout.js
+++ src/pages/packs/PaymentCheckout.js
@@ -1,31 +1,23 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import SuccessfulPayment from './SuccessfulPayment';
-import RefusedPayment from './RefusedPayment';
 import getSearchParams from '../../utils/getSearchParams';
+import PaymentStatus from '../../components/PaymentStatus'
+import { useMediaQuery } from '@mui/material';
 
 const PaymentCheckout = ({ location }) => {
     const status = getSearchParams(location, 'status')
-
-    if (status == 'success') {
-        return (
-            <Box sx={{
-                display: 'flex',
-                alignItems: 'center',
-                height: '100%'
-            }}>
-                <SuccessfulPayment />
-            </Box>
-        )
-    }
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
 
     return (
         <Box sx={{
+            flex: 1,
             display: 'flex',
-            alignItems: 'center',
-            height: '100%'
+            flexDirection: 'column',
+            alignItems: 'center'
         }}>
-            <RefusedPayment />
+            {status && (
+                <PaymentStatus isSmall={isSmall} status={status} />
+            )}
         </Box>
     );
 }
