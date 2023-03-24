69a616208064b1b8cecda1a8cdfa4b0de952a070
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-03-24 14:58:36 -0400

    add pawprints on landing page

--- src/pages/Landing/MeetDogsSection.js
+++ src/pages/Landing/MeetDogsSection.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import useMediaQuery from '@mui/material/useMediaQuery';
+import EllipseImage from '../../components/EllipseImage';
 
 const MeetDogsSection = () => {
     const isSmall = useMediaQuery((theme) => theme.breakpoints.down('sm'));
@@ -15,7 +16,8 @@ const MeetDogsSection = () => {
             display: 'flex',
             justifyContent: 'center',
             padding: '4rem 0',
-            height: isSmall ? '400px' : '500px'
+            height: isSmall ? '400px' : '500px',
+            position: 'relative'
         }}>
             <Typography
                 variant="subtitle1"
@@ -29,6 +31,48 @@ const MeetDogsSection = () => {
             >
                 Conoce las mascotas que obtuvieron más likes de la semana, día y mes
             </Typography>
+            <EllipseImage
+                n='pawprints/huella1'
+                sx={{
+                    top: '30px',
+                    right: '50px'
+                }}
+            />
+            <EllipseImage
+                n='pawprints/huella2'
+                sx={{
+                    top: '200px',
+                    right: '40px'
+                }}
+            />
+            <EllipseImage
+                n='pawprints/huella6'
+                sx={{
+                    top: '400px',
+                    right: '70px'
+                }}
+            />
+            <EllipseImage
+                n='pawprints/huella3'
+                sx={{
+                    top: '30px',
+                    left: '90px'
+                }}
+            />
+            <EllipseImage
+                n='pawprints/huella4'
+                sx={{
+                    top: '180px',
+                    left: '50px'
+                }}
+            />
+            <EllipseImage
+                n='pawprints/huella5'
+                sx={{
+                    bottom: '50px',
+                    left: '90px'
+                }}
+            />
         </Box>
     )
 }
