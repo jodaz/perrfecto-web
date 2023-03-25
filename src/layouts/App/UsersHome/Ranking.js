41527bbcd7db903ce7f65ab3e78e71f1048cbfde
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-03-25 16:55:08 -0400

    show ranking

--- src/layouts/App/UsersHome/Ranking.js
+++ src/layouts/App/UsersHome/Ranking.js
@@ -4,17 +4,31 @@ import { CircularProgress } from '@mui/material';
 import Card from '../../../components/Cards/Card';
 import { apiProvider } from '../../../api';
 import useEffectOnce from '../../../utils/useEffectOnce';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from "swiper";
+import styled from '@emotion/styled';
+
+const SwiperStyled = styled(Swiper)(() => ({
+    height: 'inherit',
+    borderRadius: 'inherit',
+    width: '100%',
+    '& .swiper-pagination-bullet': {
+        backgroundColor: '#A167C9',
+        width: '8px',
+        height: '8px',
+        boxShadow: '0px 2px 2px rgba(0, 0, 0, 0.12)'
+    }
+}))
 
 const Ranking = ({
     handleSelect,
     isSmall
 }) => {
-    const [isLoading, setIsLoading] = React.useState(false);
+    const [isLoading, setIsLoading] = React.useState(true);
     const [pubWeek, setPubWeek] = React.useState(null);
     const [pubMonth, setPubMonth] = React.useState(null);
 
     const fetchWeek = async () => {
-        setIsLoading(true);
         try {
             const res = await apiProvider.get('/api/ranking/ranking-week')
 
@@ -58,13 +72,30 @@ const Ranking = ({
             display: 'flex',
             position: 'relative',
             flexDirection: 'column',
-            width: isSmall ? '100%' : '50%',
+            width: '300px',
             margin: '0 auto',
             height: '100%',
             zIndex: 100
         }}>
             {(!isLoading) ? (
-                <></>
+                <SwiperStyled
+                    pagination={{
+                        clickable: true,
+                        dynamicBullets: false,
+                    }}
+                    modules={[Navigation, Pagination]}
+                    navigation
+                >
+                    {[pubWeek, pubMonth].map((item, index) => (
+                        <SwiperSlide key={index}>
+                            <Card
+                                data={item}
+                                drag={true}
+                                onClick={() => handleSelect(item)}
+                            />
+                        </SwiperSlide>
+                    ))}
+                </SwiperStyled>
             ) : (
                 <Box sx={{
                     height: '100%',
