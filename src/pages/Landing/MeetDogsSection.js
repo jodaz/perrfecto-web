b0bd6a3e1a87eb043fdc04a1db8ee32b29ec1256
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-03-28 00:18:44 -0400

    hide pawprints if window is too small

--- src/pages/Landing/MeetDogsSection.js
+++ src/pages/Landing/MeetDogsSection.js
@@ -31,48 +31,52 @@ const MeetDogsSection = () => {
             >
                 Conoce las mascotas que obtuvieron más likes de la semana, día y mes
             </Typography>
-            <EllipseImage
-                n='pawprints/huella1'
-                sx={{
-                    top: '30px',
-                    right: '50px'
-                }}
-            />
-            <EllipseImage
-                n='pawprints/huella2'
-                sx={{
-                    top: '200px',
-                    right: '40px'
-                }}
-            />
-            <EllipseImage
-                n='pawprints/huella6'
-                sx={{
-                    top: '400px',
-                    right: '70px'
-                }}
-            />
-            <EllipseImage
-                n='pawprints/huella3'
-                sx={{
-                    top: '30px',
-                    left: '90px'
-                }}
-            />
-            <EllipseImage
-                n='pawprints/huella4'
-                sx={{
-                    top: '180px',
-                    left: '50px'
-                }}
-            />
-            <EllipseImage
-                n='pawprints/huella5'
-                sx={{
-                    bottom: '50px',
-                    left: '90px'
-                }}
-            />
+            {!isSmall && (
+                <>
+                    <EllipseImage
+                        n='pawprints/huella1'
+                        sx={{
+                            top: '30px',
+                            right: '50px'
+                        }}
+                    />
+                    <EllipseImage
+                        n='pawprints/huella2'
+                        sx={{
+                            top: '200px',
+                            right: '40px'
+                        }}
+                    />
+                    <EllipseImage
+                        n='pawprints/huella6'
+                        sx={{
+                            top: '400px',
+                            right: '70px'
+                        }}
+                    />
+                    <EllipseImage
+                        n='pawprints/huella3'
+                        sx={{
+                            top: '30px',
+                            left: '90px'
+                        }}
+                    />
+                    <EllipseImage
+                        n='pawprints/huella4'
+                        sx={{
+                            top: '180px',
+                            left: '50px'
+                        }}
+                    />
+                    <EllipseImage
+                        n='pawprints/huella5'
+                        sx={{
+                            bottom: '50px',
+                            left: '90px'
+                        }}
+                    />
+                </>
+            )}
         </Box>
     )
 }
