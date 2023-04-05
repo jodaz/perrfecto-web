3681366032a691931a8aa1f447471d9a3c92a9c6
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-04-04 23:13:37 -0400

    add styles to scrollbar

--- src/layouts/App/Aside.js
+++ src/layouts/App/Aside.js
@@ -24,14 +24,34 @@ const Sidebar = ({ children }) => {
     return (
         <Drawer
             sx={{
-                width: drawerWidth,
-                flexShrink: 0,
                 '& .MuiDrawer-paper': {
                     width: drawerWidth,
                     boxSizing: 'border-box',
                     border: 'none'
                 },
-                zIndex: 1000
+                width: drawerWidth,
+                flexShrink: 0,
+                zIndex: 1000,
+                scrollbarColor: "#E5E5E5 #E5E5E5",
+                "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+                    backgroundColor: "#E5E5E5",
+                    width: '8px',
+                },
+                "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
+                    borderRadius: 8,
+                    backgroundColor: "#959595",
+                    minHeight: 12,
+                    border: "1px solid #959595",
+                },
+                "&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus": {
+                    backgroundColor: "#959595",
+                },
+                "&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active": {
+                    backgroundColor: "#959595",
+                },
+                "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover": {
+                    backgroundColor: "#959595",
+                },
             }}
             variant="permanent"
             anchor="left"
